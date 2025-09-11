f5ffce517fc126c10963c4320fdbd18b5232970c
ettore <ettoreitaut4.1@gmail.com>
2025-09-11 15:48:34 +0200

    Close #9 logging + new main

--- main.cc
+++ main.cc
@@ -6,22 +6,21 @@
 #include "dataset.hh"
 #include "model.hh"
 #include "mse.hh"
-#include "sgd.hh"
+// #include "sgd.hh"
+#include "loops.hh"
 // #include "linear.hh"
+#include "worker.hh"
 
 int main(int argc, char *argv[]) {
   ////////////////////////////////////////////////////////
-  experiment_config config = {
-    .train_path = "../data/mnist/train", 
-    .test_path = "../data/mnist/test",
-    .batch_size = 254,
-    .input_dim=28*28,
-    .hidden_dim=256,
-    .output_dim=28*28,
-    .lr=0.01f,
-    .epoch=100 
-  };
-
+  const experiment_config config = {.train_path = "../data/mnist/train",
+                                    .test_path = "../data/mnist/test",
+                                    .batch_size = 256,
+                                    .input_dim = 28 * 28,
+                                    .hidden_dim = 256,
+                                    .output_dim = 28 * 28,
+                                    .lr = 0.01f,
+                                    .epoch = 100};
 
   // const int max_batch_size{254};
   // const int input_dim{28 * 28};
@@ -36,57 +35,42 @@ int main(int argc, char *argv[]) {
   auto [train_filenames, eval_filenames] =
       random_split_filenames(filenames, 20, 42);
 
-  std::string test_path = "../data/mnist/test";
-  std::vector<std::string> test_filenames = get_filenames(test_path);
+  // std::string test_path = "../data/mnist/test";
+  std::vector<std::string> test_filenames = get_filenames(config.test_path);
 
-  Dataloader train_dataloader(config.train_path, train_filenames, 28, 28,
-                              train_filenames.size(), config.batch_size, true);
-  Dataloader eval_dataloader(config.train_path, eval_filenames, 28, 28,
-                             eval_filenames.size(), config.batch_size, false);
-  Dataloader test_dataloader(test_path, test_filenames, 28, 28,
-                             test_filenames.size(), config.batch_size, false);
+  // Dataloader train_dataloader(config.train_path, train_filenames, 28, 28,
+  //                             train_filenames.size(), config.batch_size,
+  //                             true);
+  // Dataloader eval_dataloader(config.train_path, eval_filenames, 28, 28,
+  //                            eval_filenames.size(), config.batch_size,
+  //                            false);
+  // Dataloader test_dataloader(config.test_path, test_filenames, 28, 28,
+  //                            test_filenames.size(), config.batch_size,
+  //                            false);
 
-  std::cout << "Got dataloaders" << std::endl;
+  // std::cout << "Got dataloaders" << std::endl;
 
-  //////////////////////////////////////////////////////////
+  // //////////////////////////////////////////////////////////
 
-  AutoencoderModel model{config.batch_size, config.input_dim, config.hidden_dim, config.output_dim};
-  // std::cout << "Created model" << std::endl;
+  // AutoencoderModel model{config.batch_size, config.input_dim,
+  // config.hidden_dim,
+  //                        config.output_dim};
+  // MSE criterion{config.batch_size, config.input_dim};
 
-  // return 1;
+  // /////////////////////////////////////////////////////////
 
-  // // for (auto batch = dataloader.begin(); batch != dataloader.end();
-  // ++batch) int counter = 0;
+  auto_worker(config, train_filenames, eval_filenames, test_filenames, "prove", 0);
+  // auto_worker(config);
+  // for (int epoch = 0; epoch < config.epoch; ++epoch) {
+  //   float train_loss = train(config, train_dataloader, model, criterion);
 
-  MSE criterion{config.batch_size, config.input_dim};
+  //   float test_loss = test("Eval:", eval_dataloader, model, criterion);
 
-  /////////////////////////////////////////////////////////
+  //   std::cout << "train_loss: " << train_loss << "\n";
 
+  //   break;
+  // }
 
-  for (int epoch = 0; epoch < 100; ++epoch) {
-    // TODO: collect metrics, etc
-
-    float epoch_loss{0};
-    int num_batches{0};
-
-    for (auto &batch : train_dataloader) {
-      auto prediction = model.forward(batch);
-      auto loss = criterion.mse_loss(batch, prediction);
-      auto grad = criterion.mse_gradient(batch, prediction);
-      model.backward(batch, grad);
-      sgd(config.lr, model.encoder, model.decoder);
-
-      num_batches++;
-      epoch_loss += loss;
-
-      printf("Batch %i/%i | Loss: %.3f\n", num_batches,
-             train_dataloader.get_num_batches(), loss);
-    }
-
-    // float average_epoch_loss = epoch_loss / num_batches;
-    
-    break;
-  }
   std::cout << "Done!" << std::endl;
 
   return 0;
--- src/model/model.hh
+++ src/model/model.hh
@@ -1,64 +1,67 @@
 #ifndef __AUTOENCODER_MODEL_HH__
 #define __AUTOENCODER_MODEL_HH__
 
-#include <Eigen/Dense>
-#include "Eigen/src/Core/Matrix.h"
-#include "relu.hh"
 #include "linear.hh"
-
-class AutoencoderModel
-{
+#include "relu.hh"
+#include <Eigen/Dense>
+/**
+ * Simple autoencoder model with linear input and output.
+ *
+ * Consider that this works without any special management both for
+ * training and inference. The only thing you need to care about is
+ * making sure that there's no concurrency (e.g. training and testing
+ * at the same time), otherwise the temp. values will explode (overwritten).
+ *
+ * Have fun! - E
+ */
+class AutoencoderModel {
 private:
-    size_t max_batch_size;
-    size_t input_dim;
-    size_t hidden_dim;
-    size_t output_dim;
+  size_t max_batch_size;
+  size_t input_dim;
+  size_t hidden_dim;
+  size_t output_dim;
 
 public:
-    Linear encoder{max_batch_size, input_dim, hidden_dim};
-    ReLU encoder_activation{max_batch_size, hidden_dim};
-    Linear decoder{max_batch_size, hidden_dim, output_dim};
-    ReLU decoder_activation{max_batch_size, output_dim};
+  Linear encoder{max_batch_size, input_dim, hidden_dim};
+  ReLU encoder_activation{max_batch_size, hidden_dim};
+  Linear decoder{max_batch_size, hidden_dim, output_dim};
+  ReLU decoder_activation{max_batch_size, output_dim};
 
-    // // Cached activations from the forward pass
-    Eigen::MatrixXf encoded;
-    Eigen::MatrixXf activated_encoded;
-    Eigen::MatrixXf decoded;
-    Eigen::MatrixXf activated_decoded;
+  // // Cached activations from the forward pass
+  Eigen::MatrixXf encoded;
+  Eigen::MatrixXf activated_encoded;
+  Eigen::MatrixXf decoded;
+  Eigen::MatrixXf activated_decoded;
 
-    AutoencoderModel(
-        size_t max_batch_size, 
-        size_t input_dim, 
-        size_t hidden_dim, 
-        size_t output_dim) : 
-        max_batch_size(max_batch_size), 
-        input_dim(input_dim), 
-        hidden_dim(hidden_dim), 
-        output_dim(output_dim),
+  AutoencoderModel(size_t max_batch_size, size_t input_dim, size_t hidden_dim,
+                   size_t output_dim)
+      : max_batch_size(max_batch_size), input_dim(input_dim),
+        hidden_dim(hidden_dim), output_dim(output_dim),
         encoder(max_batch_size, input_dim, hidden_dim),
         encoder_activation(max_batch_size, hidden_dim),
         decoder(max_batch_size, hidden_dim, output_dim),
-        decoder_activation(max_batch_size, output_dim)    
-    {}
+        decoder_activation(max_batch_size, output_dim) {}
 
-    Eigen::MatrixXf forward(const Eigen::MatrixXf &input)
-    {
-        encoded = encoder.forward(input);
-        activated_encoded = encoder_activation.forward(encoded);
-        decoded = decoder.forward(activated_encoded);
-        activated_decoded = decoder_activation.forward(decoded);
-        return activated_decoded;
-    }
+  Eigen::MatrixXf forward(const Eigen::MatrixXf &input) {
+    encoded = encoder.forward(input);
+    activated_encoded = encoder_activation.forward(encoded);
+    decoded = decoder.forward(activated_encoded);
+    activated_decoded = decoder_activation.forward(decoded);
+    return activated_decoded;
+  }
 
-    Eigen::MatrixXf backward(const Eigen::MatrixXf &input, const Eigen::MatrixXf &grad_output)
-    {
-        Eigen::MatrixXf grad_decoded = decoder_activation.backward(decoded, grad_output);
-        Eigen::MatrixXf grad_hidden = decoder.backward(activated_encoded, grad_decoded);
-        Eigen::MatrixXf grad_activated_hidden = encoder_activation.backward(encoded, grad_hidden);
-        Eigen::MatrixXf grad_input = encoder.backward(input, grad_activated_hidden);
-        
-        return grad_input;
-    }
+  Eigen::MatrixXf backward(const Eigen::MatrixXf &input,
+                           const Eigen::MatrixXf &grad_output) {
+    Eigen::MatrixXf grad_decoded =
+        decoder_activation.backward(decoded, grad_output);
+    Eigen::MatrixXf grad_hidden =
+        decoder.backward(activated_encoded, grad_decoded);
+    Eigen::MatrixXf grad_activated_hidden =
+        encoder_activation.backward(encoded, grad_hidden);
+    Eigen::MatrixXf grad_input = encoder.backward(input, grad_activated_hidden);
+
+    return grad_input;
+  }
 };
 
 #endif // __AUTOENCODER_MODEL_HH__
